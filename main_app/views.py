4b1472258152fd0593694dd31e927b0b22ec5e1b
Alexander <alexgeniusman@gmail.com>
2021-11-13 01:58:04 +0300

    uvicon updated

--- main_app/views.py
+++ main_app/views.py
@@ -15,7 +15,10 @@ from .ml_engine import search
 
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 async def index(request):
     return HttpResponse("Hello, async Django!")
 
