27307e60aa4fc323912186bed09419cf3a658980
Nikita Moroshkin <whiteandblackreality@gmail.com>
2021-11-13 18:07:45 +0300

    add time check + speller

--- main_app/ml_engine.py
+++ main_app/ml_engine.py
@@ -33,69 +33,12 @@ layout_ru_en = dict(zip(map(ord, "йцукенгшщзхъфывапролджэ
                         "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
                         'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'))
 
-# # Объект орфографа русского и английского языка
-# spell_en = Speller('en')
-# spell_ru = Speller('ru')
+# Объект орфографа русского и английского языка
+spell_en = Speller('en')
+spell_ru = Speller('ru')
 
 query_popularity = pd.read_csv('query_popularity.csv')
 data_most_common = np.char.lower(np.array(query_popularity['query'], dtype=np.str_))
-
-
-def change_layout(query):
-    """
-    Изменение раскладки с английских символов на русские и с русских на английские
-    :param query: поисковый запрос (str)
-    :return: поисковый запрос с измененной раскладкой на русский язык и на английский язык в виде кортежа (tuple(str))
-    """
-    reg = re.compile("\'|\)|\(|\,")
-
-    query_split = query.split()
-    words_query = {}
-
-    i = 0
-    output = []
-    n_output = 1
-
-    for word in query_split:
-        words_query[i] = (list(np.unique([word, word.translate(layout_en_ru), (word.translate(layout_ru_en))])))
-        i += 1
-
-    for word in words_query.values():
-        n_output *= len(word)
-
-    if i != 1:
-
-        for j in range(1, len(words_query)):
-            if j == 1:
-                for _ in product(words_query[j - 1], words_query[j]):
-                    output.append(_)
-            else:
-                for _ in product(output, words_query[j]):
-                    output.append(_)
-
-        for n_varient in range(len(output)):
-            output[n_varient] = re.sub("\'|\)|\(|\,", '', str(output[n_varient])).replace(' ', '\s')
-
-        return output[-n_output:]
-
-    else:
-
-        return words_query[0]
-
-
-# def correct_query_autocorrect(queries):
-#     """
-#     Исправление опечаток запроса на русском языке
-#     :param query: поисковый запрос (str)
-#     :return: исправленный запрос (str)
-#     """
-#     result = []
-#     for query in queries:
-#         result.append(spell_en(query))
-#         result.append(spell_ru(query))
-#     result = np.unique(np.array(result, dtype=np.str_))
-#     result = '|'.join(result)
-#     return result
 
 
 def correct_query(query):
@@ -127,18 +70,22 @@ def correct_query(query):
                     output = np.append(output, str(_).replace('"', '').replace('\\', ''))
 
         output = output[-n_output:]
+        start_time = time.time()
         for n_varient in range(len(output)):
             output[n_varient] = re.sub("\'|\)|\(|\,", '', output[n_varient])
 
-            # output[n_varient] = spell_ru(output[n_varient]).replace(' ', '\s')
-            output[n_varient] = output[n_varient].replace(' ', '\s')
+            output[n_varient] = spell_ru(output[n_varient]).replace(' ', '\s')
+            # output[n_varient] = output[n_varient].replace(' ', '\s')
+        logger.info(f'Затраченное время на исправление опечаток: {time.time() - start_time}')
 
 
     else:
 
+        start_time = time.time()
         for word in words_query[0]:
-            # output = np.append(output, str(spell_ru(word)))
-            output = np.append(output, str(word))
+            output = np.append(output, str(spell_ru(word)))
+            # output = np.append(output, str(word))
+        logger.info(f'Затраченное время на исправление опечаток: {time.time() - start_time}')
 
     return '(' + ')|('.join(output) + ')'
 
@@ -161,12 +108,14 @@ def search(status_autorization, query, n_query):
         start_time = time.time()
         reg = re.compile('[^\|a-zа-я0-9\\\s\^]')
         re_query = reg.sub('', correct_query(query))
+        logger.info(f'Затраченное время на обработку запроса: {time.time()-start_time}')
+        start_time = time.time()
         for string in data_most_common:
             result_one_string = re.findall(re_query, string, flags=re.IGNORECASE)
             if result_one_string != list():
                 result.append(string)
         logger.info(f'Найдено подсказок: {len(result)}')
-        logger.info(f'Затраченное время на обработку запроса: {time.time()-start_time}')
+        logger.info(f'Затраченное время на поиск подсказок: {time.time()-start_time}')
     else:
         # Анонимный пользователь
         logger.debug('Начинается поиск подсказок анонимному пользователю')
