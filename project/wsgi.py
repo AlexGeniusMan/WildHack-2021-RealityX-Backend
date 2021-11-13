7c1cb19720bdd1e72b06460bedd1d20f50cf23fe
Alexander <alexgeniusman@gmail.com>
2021-11-13 16:20:46 +0300

    gunicorn optimizing

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,31 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.conf import settings
+from django.test import RequestFactory
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+
+# make request to /api/v1/status to prepare everything for first user request
+def make_init_request():
+    f = RequestFactory()
+    request = f.request(**{
+        'wsgi.url_scheme': 'http',
+        'HTTP_HOST': '80.78.244.130',
+        'QUERY_STRING': '',
+        'REQUEST_METHOD': 'POST',
+        'PATH_INFO': '/api/predict_hints',
+        'SERVER_PORT': '8001',
+    })
+
+    def start_response(*args):
+        pass
+
+    application(request.environ, start_response)
+
+
+if os.environ.get('WSGI_FULL_INIT'):
+    make_init_request()
