7613a869dcbf67baf0f5437f33f3782e3242a302
Alexander <alexgeniusman@gmail.com>
2021-11-13 17:01:20 +0300

    gunicorn optimizing

--- project/urls.py
+++ project/urls.py
@@ -25,7 +25,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 import main_app.views as views
-from main_app.views import index
 
 schema_view = get_schema_view(
     openapi.Info(
